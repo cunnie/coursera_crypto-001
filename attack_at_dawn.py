204360ce5a3e955a323097178b2b1ead1bb48fff
Brian Cunnie <brian.cunnie@gmail.com>
2015-01-17 11:18:36 -0800

    Leading zeroes will get you every time

--- attack_at_dawn.py
+++ attack_at_dawn.py
@@ -3,6 +3,6 @@ import sys
 
 old_plaintext=0x61747461636b206174206461776e
 new_plaintext=0x61747461636b206174206475736b
-cipher_text=  0x6c73d5240a948c86981bc294814d
+cipher_text=  0x09e1c5f70a65ac519458e7e53f36
 
 print hex(old_plaintext ^ cipher_text ^ new_plaintext)
